b5f2bcc9e3e10e20967c8622b0026286d5d7fbae
Janaina P de Oliveira Minutti <janainapp@gmail.com>
2022-08-22 22:31:12 -0300

    simple fighter interface creation and fighter refactoring

---
+++ src/Character.ts
@@ -0,0 +1,91 @@
+import Archetype, { Mage } from './Archetypes';
+import Energy from './Energy';
+import Fighter from './Fighter';
+import Race, { Elf } from './Races';
+import getRandomInt from './utils';
+
+const generateRandomNumber = getRandomInt(1, 10);
+
+class Character implements Fighter {
+  private _race: Race;
+  private _archetype: Archetype;
+  private _maxLifePoints: number;
+  private _lifePoints: number;
+  private _strength: number;
+  private _defense: number;
+  private _dexterity: number;
+  private _energy: Energy;
+  private _name: string;
+
+  constructor(name: string) {
+    this._name = name;
+    this._dexterity = generateRandomNumber;
+    this._race = new Elf(name, this._dexterity);
+    this._archetype = new Mage(name);
+    this._maxLifePoints = this._race.maxLifePoints / 2;
+    this._lifePoints = this._maxLifePoints;
+    this._strength = generateRandomNumber;
+    this._defense = generateRandomNumber;
+    this._energy = {
+      type_: this._archetype.energyType,
+      amount: generateRandomNumber,
+    };
+  }
+
+  public get race(): Race {
+    return this._race;
+  }
+
+  public get archetype(): Archetype {
+    return this._archetype;
+  }
+
+  public get lifePoints(): number {
+    return this._lifePoints;
+  }
+
+  public get strength(): number {
+    return this._strength;
+  }
+
+  public get defense(): number {
+    return this._defense;
+  }
+
+  public get dexterity(): number {
+    return this._dexterity;
+  }
+
+  public get energy(): Energy {
+    return {
+      type_: this.energy.type_,
+      amount: this.energy.amount,
+    };
+  }
+
+  receiveDamage(attackPoints: number): number {
+    const damage = attackPoints - this._defense;
+    if (damage < 0) this._lifePoints -= damage;
+    if (this._lifePoints < 1) this._lifePoints = -1;
+    return this._lifePoints;
+  }
+
+  attack(enemy: Fighter): void {
+    enemy.receiveDamage(this._strength);
+  }
+
+  levelUp(): void {
+    this._maxLifePoints += generateRandomNumber;
+    this._strength += generateRandomNumber;
+    this._dexterity += generateRandomNumber;
+    this._defense += generateRandomNumber;
+    this._energy.amount = 10;
+
+    if (this._maxLifePoints > this._race.maxLifePoints) {
+      this._maxLifePoints = this._race.maxLifePoints;
+    }
+    this._lifePoints = this._maxLifePoints;
+  }
+}
+
+export default Character;
--- src/Fighter/Fighter.ts
+++ src/Fighter/Fighter.ts
@@ -1,16 +1,12 @@
 import Energy from '../Energy';
 
 interface Fighter {
-  lifePoints: number;
-  strength: number;
   defense: number;
   energy?: Energy;
 
-  attack(enemy: Fighter): void;
   special?(nemy: Fighter): void;
   levelUp(): void;
-  receiveDamage(attackPoints: number): number;
-
+  
 } 
 
 export default Fighter;
